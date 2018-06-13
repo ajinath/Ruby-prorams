aaa1505e7c132ad6e2ac355b75d9a5bdc2837f3f
ajinath <ajinathjedhe99@gmail.com>
2018-06-13 16:30:23 +0530

    Create roman_to_decimal.rb

---
+++ roman_to_decimal.rb
@@ -0,0 +1,12 @@
+ROMAN = {
+  'M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100,
+  'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10,
+  'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1
+}
+
+def solution(roman)
+  re = Regexp.new(ROMAN.keys.join('|'))
+  roman.scan(re).inject(0) do |number, key|
+    number + ROMAN[key]
+  end
+end
